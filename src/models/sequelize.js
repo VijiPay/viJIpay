395f3248af0ed1a2a0fb59c34425a7514f942768
shemzz <davidshemang@gmail.com>
2024-02-12 17:35:14 +0100

    add dialectOptions

--- src/models/sequelize.js
+++ src/models/sequelize.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
-let { HOST, DATABASE, DBUSER, DBPASSWORD} = process.env;
+
+let { HOST, DATABASE, DBUSER, DBPASSWORD } = process.env;
 
 const sequelize = new Sequelize(DATABASE, DBUSER, DBPASSWORD, {
     host: HOST,
@@ -12,6 +13,13 @@ const sequelize = new Sequelize(DATABASE, DBUSER, DBPASSWORD, {
         min: 0,
         acquire: 30000,
         idle: 20000,
+    },
+    port: 5432,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // set this to true in production after configuring SSL certificates
+        },
     },
 });
 
